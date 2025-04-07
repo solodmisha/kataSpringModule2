43e114656a93da2baa412ae5ae613b4de1c4ef54
solodmisha <michaelsolodeynikov@gmail.com>
2025-04-07 16:33:10 +0300

    First commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,38 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User findUserByCarModelAndSeries(String model, int series) {
+      return (User) sessionFactory.getCurrentSession()
+              .createQuery("FROM User u WHERE u.car.model = :model AND u.car.series = :series")
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .uniqueResult();
+   }
+
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,59 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private String series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {}
+
+    public Car(String model, String series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
